6b5bd807594caef6e635c4d9c8e8f57004cf7f33
ViralSir <viralsir2018@gmail.com>
2024-01-17 17:04:24 +0530

    Lecture : course_list.html and urls.py has been added.

--- course/urls.py
+++ course/urls.py
@@ -3,4 +3,9 @@ from .views import *
 urlpatterns=[
         path("",home,name="course-home"),
         path("new/",NewCourse.as_view(),name='course-new'),
+        path("view/",ViewCourse.as_view(),name='course-view'),
+        path("update/<int:pk>",UpdateCourse.as_view(),name="course-update"),
+        path("delete/<int:pk>",DeleteCourse.as_view(),name="course-delete"),
+        path("detail/<int:pk>",DetailCourse.as_view(),name="course-detail"),
+
 ]
